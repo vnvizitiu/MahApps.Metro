58f360054435088f4980ddfb31a3f1c54076b30c
Jan Karger <punkerat76@gmail.com>
2015-12-10 17:11:35 +0100

    new ContextMenuHelper class

---
+++ MahApps.Metro/Controls/Helper/ContextMenuHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace MahApps.Metro.Controls
+{
+    public class ContextMenuHelper
+    {
+        public static readonly DependencyProperty IsContextMenuBoundProperty
+            = DependencyProperty.RegisterAttached(
+                "IsContextMenuBound",
+                typeof(bool?),
+                typeof(ContextMenuHelper),
+                new FrameworkPropertyMetadata(default(bool?)));
+
+        public static void SetIsContextMenuBound(DependencyObject element, bool? value)
+        {
+            element.SetValue(IsContextMenuBoundProperty, value);
+        }
+
+        public static bool? GetIsContextMenuBound(DependencyObject element)
+        {
+            return (bool?)element.GetValue(IsContextMenuBoundProperty);
+        }
+
+        public static readonly DependencyProperty IsContextMenuVisibleProperty
+            = DependencyProperty.RegisterAttached(
+                "IsContextMenuVisible",
+                typeof(bool?),
+                typeof(ContextMenuHelper),
+                new FrameworkPropertyMetadata((bool?)true));
+
+        public static void SetIsContextMenuVisible(DependencyObject element, bool? value)
+        {
+            element.SetValue(IsContextMenuVisibleProperty, value);
+        }
+
+        public static bool? GetIsContextMenuVisible(DependencyObject element)
+        {
+            return (bool?)element.GetValue(IsContextMenuVisibleProperty);
+        }
+    }
+}
