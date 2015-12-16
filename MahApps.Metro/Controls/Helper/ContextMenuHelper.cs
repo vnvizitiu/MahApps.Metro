76bcb1aa17bdd7e067913b7377a36a20b2bd48e3
Jan Karger <punkerat76@gmail.com>
2015-12-16 09:10:03 +0100

    not working

--- MahApps.Metro/Controls/Helper/ContextMenuHelper.cs
+++ MahApps.Metro/Controls/Helper/ContextMenuHelper.cs
@@ -26,7 +26,27 @@ namespace MahApps.Metro.Controls
                 "IsContextMenuVisible",
                 typeof(bool?),
                 typeof(ContextMenuHelper),
-                new FrameworkPropertyMetadata((bool?)true));
+                new FrameworkPropertyMetadata((bool?)true, IsContextMenuVisibleChanged));
+
+        private static void IsContextMenuVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var fe = d as FrameworkElement;
+            if (fe != null && e.OldValue != e.NewValue)
+            {
+                fe.ContextMenuOpening -= FrameworkElementContextMenuOpening;
+                fe.ContextMenuOpening += FrameworkElementContextMenuOpening;
+            }
+        }
+
+        private static void FrameworkElementContextMenuOpening(object sender, System.Windows.Controls.ContextMenuEventArgs e)
+        {
+            var fe = (FrameworkElement)sender;
+            var isContextMenuVisible = GetIsContextMenuVisible(fe).GetValueOrDefault(true);
+            if (!isContextMenuVisible)
+            {
+                fe.ContextMenu = null;
+            }
+        }
 
         public static void SetIsContextMenuVisible(DependencyObject element, bool? value)
         {
